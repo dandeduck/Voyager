20ad66df8c45782e8a7ea56a33fc9d1735337e70
Tom Tzook <tomtzook@gmail.com>
2020-08-06 00:59:06 +0300

    fixed naming and packages

--- app/src/main/java/com/navapp/MainActivity.java
+++ app/src/main/java/com/navapp/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.unnamed;
+package com.navapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.unnamed.R;
 
 public class MainActivity extends AppCompatActivity {
 
