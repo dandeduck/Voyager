eb43b1a8ea3c06f1dc9ee159d737e9a4a47aa1e3
dandeduck <danielmkhlv@gmail.com>
2020-08-06 20:45:01 +0300

    some more stuff in route

--- app/src/main/java/com/navapp/navigation/route/Route.java
+++ app/src/main/java/com/navapp/navigation/route/Route.java
@@ -7,15 +7,25 @@ import androidx.annotation.RequiresApi;
 import com.navapp.navigation.destination.Destination;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class Route {
     private final ArrayDeque<Destination> destinations;
-    private final ArrayDeque<Destination> destinationsCopy;
+    private final List<Destination> destinationsCopy;
 
     public Route(Collection<Destination> destinations) {
         this.destinations = new ArrayDeque<>(destinations);
-        destinationsCopy = this.destinations.clone();
+        destinationsCopy = new ArrayList<>(destinations);
+    }
+
+    public ArrayDeque<Destination> destinationsLeft() {
+        return destinations.clone();
+    }
+
+    public List<Destination> allDestinations() {
+        return destinationsCopy;
     }
 
     public Destination nextDestination() {
@@ -24,11 +34,13 @@ public class Route {
 
     public void addDestination(Destination destination) {
         destinations.push(destination);
+        destinationsCopy.add(destination);
         optimize();
     }
 
     public void optimize() {
         //do stuff
+        //also may be optimize the copy (if it's not too slow...)
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
