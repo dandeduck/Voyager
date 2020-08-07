8018cd9baea2526647c6b3f88e8c04bfd47f519c
dandeduck <danielmkhlv@gmail.com>
2020-08-07 20:29:03 +0300

    fixed some stuff in route handler

--- app/src/main/java/com/navapp/navigation/route/RouteHandler.java
+++ app/src/main/java/com/navapp/navigation/route/RouteHandler.java
@@ -1,22 +1,13 @@
 package com.navapp.navigation.route;
 
-import android.os.Build;
-
-import androidx.annotation.RequiresApi;
-
 import com.navapp.navigation.destination.Destination;
 import com.navapp.navigation.destination.data.Address;
 
-import java.util.List;
-import java.util.function.Supplier;
-
 public class RouteHandler {
-    private final Supplier<Address> locationSupplier;
     private Route route;
     private int currentDestination;
 
-    public RouteHandler(Supplier<Address> locationSupplier, Route route) {
-        this.locationSupplier = locationSupplier;
+    public RouteHandler(Route route) {
         this.route = route;
         currentDestination = -1;
     }
@@ -24,15 +15,6 @@ public class RouteHandler {
     public Destination nextDestination() {
         currentDestination++;
         return route.destinations().get(currentDestination);
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    public Route addDestination(Destination destination) {
-        List<Destination> destinations = route.destinations();
-        destinations.add(destination);
-        route = new Route(destinations, route.end());
-        
-        return optimizeRoute(locationSupplier.get());
     }
 
     public Route optimizeRoute(Address start) {
