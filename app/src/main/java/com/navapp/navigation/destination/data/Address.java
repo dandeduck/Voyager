1bdd16f7deaa66e502183a526820cee3338274e5
dandeduck <danielmkhlv@gmail.com>
2020-08-07 16:39:56 +0300

    removed the unimplemented ctor from Address

--- app/src/main/java/com/navapp/navigation/destination/data/Address.java
+++ app/src/main/java/com/navapp/navigation/destination/data/Address.java
@@ -4,10 +4,6 @@ public class Address {
     private final double latitude;
     private final double longitude;
 
-    public Address(String addressText) {
-        this(0, 0); //implement find through Waze.
-    }
-
     public Address(double latitude, double longitude) {
         this.latitude = latitude;
         this.longitude = longitude;
