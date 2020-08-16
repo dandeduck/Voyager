d297a71397a12f30d5b69e49d51f7c1115bc5bba
dandeduck <danielmkhlv@gmail.com>
2020-08-16 18:12:43 +0300

    simplified Locaiton

--- app/src/main/java/com/navapp/navigation/destination/data/Location.java
+++ app/src/main/java/com/navapp/navigation/destination/data/Location.java
@@ -1,31 +1,13 @@
 package com.navapp.navigation.destination.data;
 
-import android.location.Address;
-import android.location.Geocoder;
-
-import java.io.IOException;
-import java.util.List;
-
 public class Location {
-    private final Address address;
+    private final String id;
 
-    public Location(Geocoder geocoder, String addressName, int wantedResult) throws IOException {
-        this(geocoder.getFromLocationName(addressName, wantedResult + 1), wantedResult);
+    public Location(String id) {
+        this.id = id;
     }
 
-    public Location(Geocoder geocoder, double latitude, double longitude, int wantedResult) throws IOException {
-        this(geocoder.getFromLocation(latitude, longitude, wantedResult + 1), wantedResult);
-    }
-
-    public Location(List<Address> results, int wantedResult) {
-        this(results.get(wantedResult));
-    }
-
-    public Location(Address address) {
-        this.address = address;
-    }
-
-    public Address getAddress() {
-        return address;
+    public String getId() {
+        return id;
     }
 }
