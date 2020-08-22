9b72a7dfc61f206e8d93f1f9a71f8c27fed3a6fa
dandeduck <danielmkhlv@gmail.com>
2020-08-22 22:54:46 +0300

    added phone number test

--- app/src/androidTest/java/com/navapp/navigation/destination/data/PhoneNumberTest.java
+++ app/src/androidTest/java/com/navapp/navigation/destination/data/PhoneNumberTest.java
@@ -1,4 +1,16 @@
 package com.navapp.navigation.destination.data;
 
+import org.junit.Test;
+
 public class PhoneNumberTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void create_tooShortNumber_throwsException() {
+        new PhoneNumber("12345");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void create_tooLongNumber_throwsException() {
+        new PhoneNumber("1234567890123456");
+    }
 }
