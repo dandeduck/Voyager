1d8721c5a2ce70dbb61236a2796a465a60e39a69
dandeduck <danielmkhlv@gmail.com>
2020-08-23 17:51:24 +0300

    removed API use in upon arrival

--- app/src/main/java/com/navapp/functional/ConditionalFunction.java
+++ app/src/main/java/com/navapp/functional/ConditionalFunction.java
@@ -1,15 +1,11 @@
 package com.navapp.functional;
 
-import com.google.maps.errors.ApiException;
-
-import java.io.IOException;
-
 public interface ConditionalFunction {
-    default void check() throws InterruptedException, ApiException, IOException {
+    default void check(){
         if(condition())
             function();
     }
 
-    boolean condition() throws InterruptedException, ApiException, IOException;
+    boolean condition();
     void function();
 }
--- app/src/main/java/com/navapp/navigation/util/AtomicUponArrival.java
+++ app/src/main/java/com/navapp/navigation/util/AtomicUponArrival.java
@@ -1,16 +1,15 @@
 package com.navapp.navigation.util;
 
-import com.google.maps.GeoApiContext;
 import com.google.maps.model.LatLng;
 
 import java.util.concurrent.atomic.AtomicReference;
 
-public abstract class AtomicUponArrival extends UponArrival {
+public abstract class AtomicUponArrival extends SimpleUponArrival {
     private final AtomicReference<LatLng> currentLocation;
     private final AtomicReference<LatLng> nextLocation;
 
-    protected AtomicUponArrival(GeoApiContext context, long minDurationInSeconds, AtomicReference<LatLng> currentLocation, AtomicReference<LatLng> nextLocation) {
-        super(context, minDurationInSeconds);
+    protected AtomicUponArrival(long minDurationInSeconds, AtomicReference<LatLng> currentLocation, AtomicReference<LatLng> nextLocation) {
+        super(minDurationInSeconds);
         this.currentLocation = currentLocation;
         this.nextLocation = nextLocation;
     }
