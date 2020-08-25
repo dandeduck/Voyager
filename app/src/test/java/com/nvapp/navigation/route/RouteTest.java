e7f2b9a5a8f5fdddeb0cc9bf80ff208f1c8d2290
dandeduck <danielmkhlv@gmail.com>
2020-08-25 20:35:03 +0300

    moved and expanded phone number tests

--- app/src/test/java/com/nvapp/navigation/route/RouteTest.java
+++ app/src/test/java/com/nvapp/navigation/route/RouteTest.java
@@ -18,19 +18,14 @@ import static org.junit.Assert.assertEquals;
 public class RouteTest {
     private final static double DEFAULT_DELTA = 0.001;
     private final static Destination BASE_DESTINATION = new Destination(new Location(""));
-    private final static Rate FIRST_RATE = new Rate("first", 10);
-    private final static Rate SECOND_RATE = new Rate("second", 20);
-    private final static Rate THIRD_RATE = new Rate("third", 30);
-
-    private Route route;
-
-    @Before
-    public void createRoute() {
-        route = new Route(BASE_DESTINATION.changeRate(FIRST_RATE), BASE_DESTINATION.changeRate(SECOND_RATE), BASE_DESTINATION.changeRate(THIRD_RATE), BASE_DESTINATION);
-    }
 
     @Test
     public void calcRateSum_returnsRateSum() {
+        final Rate FIRST_RATE = new Rate("first", 10);
+        final Rate SECOND_RATE = new Rate("second", 20);
+        final Rate THIRD_RATE = new Rate("third", 30);
+        Route route = new Route(BASE_DESTINATION.changeRate(FIRST_RATE), BASE_DESTINATION.changeRate(SECOND_RATE), BASE_DESTINATION.changeRate(THIRD_RATE), BASE_DESTINATION);
+        
         assertEquals(FIRST_RATE.getValue() + SECOND_RATE.getValue() + THIRD_RATE.getValue(), route.calcRateSum(), DEFAULT_DELTA);
     }
 }
