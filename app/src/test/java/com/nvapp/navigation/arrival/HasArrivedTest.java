325f93a73d5f784c9635391b5ff500466090460f
dandeduck <danielmkhlv@gmail.com>
2020-08-29 17:08:54 +0300

    changed has arrived test to use verify

--- app/src/test/java/com/nvapp/navigation/arrival/HasArrivedTest.java
+++ app/src/test/java/com/nvapp/navigation/arrival/HasArrivedTest.java
@@ -6,21 +6,20 @@ import org.junit.Test;
 
 import java.util.Observer;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 public class HasArrivedTest {
     @Test
     public void observeHasArrived_whenUpdated_ObserverIsTriggered() {
-        Observer checker = (observable, o) -> {
-            throw new IllegalStateException();
-        };
+        Observer checker = mock(Observer.class);
         HasArrived hasArrived = HasArrived.getInstance();
         hasArrived.addObserver(checker);
 
-        try {
-            hasArrived.update();
-            fail();
-        } catch (Exception ignored){}
+        hasArrived.update();
+        verify(checker, times(1)).update(any(), any());
     }
 }
