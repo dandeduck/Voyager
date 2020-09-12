eea4be94ef64a2fbd3b331e51d029f672667b1e5
dandeduck <danielmkhlv@gmail.com>
2020-09-13 00:49:26 +0300

    changed route linnk factory to rely on the builder

--- app/src/test/java/com/nvapp/navigation/route/RouteLinkFactoryTest.java
+++ app/src/test/java/com/nvapp/navigation/route/RouteLinkFactoryTest.java
@@ -1,13 +1,11 @@
 package com.nvapp.navigation.route;
 
-import android.net.Uri;
-
 import com.google.maps.model.LatLng;
 import com.navapp.navigation.destination.Destination;
 import com.navapp.navigation.destination.data.Place;
 import com.navapp.navigation.route.Route;
+import com.navapp.navigation.route.RouteLinkBuilder;
 import com.navapp.navigation.route.RouteLinkFactory;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,19 +19,11 @@ import static org.junit.Assert.assertEquals;
 @RunWith(RobolectricTestRunner.class)
 @Config(manifest = Config.NONE)
 public class RouteLinkFactoryTest {
-    private static final String NAVIGATION_LINK =  "https://www.google.com/maps/dir/?api=1";
-    private static final String ORIGIN_TEMPLATE = "&origin=%s&origin_place_id=%s";
-    private static final String DESTINATION_TEMPLATE = "&destination=%s&destination_place_id=%s";
-    private static final String WAYPOINT_TEMPLATE = "&waypoints=%s|%s&waypoint_place_ids=%s|%s";
-
-    private static final String DRIVING_TRAVEL_MODE = "&travelmode=driving";
-    private static final String SKIP_OVERVIEW = "&dir_action=navigate";
-
     private Route route;
-    private String baseExpectedLinkText;
+    private RouteLinkBuilder linkBuilder;
 
     @Before
-    public void createRoute() {
+    public void createRouteAndRouteLinkBuilder() {
         ArrayList<Destination> destinations = new ArrayList<>();
         destinations.add(new Destination(new Place("1", "address1", new LatLng(0, 0))));
         destinations.add(new Destination(new Place("2", "address2", new LatLng(0, 0))));
@@ -42,21 +32,24 @@ public class RouteLinkFactoryTest {
 
         route = new Route(destinations);
 
-        baseExpectedLinkText = String.format(NAVIGATION_LINK + ORIGIN_TEMPLATE + DESTINATION_TEMPLATE + WAYPOINT_TEMPLATE, "address1", "1", "address4", "4","address2", "address3", "2", "3");
+        linkBuilder = new RouteLinkBuilder()
+                .origin(route)
+                .destination(route)
+                .waypoints(route);
     }
 
     @Test
     public void create_falseFlags_returnCorrectUri() {
-        assertEquals(Uri.parse(baseExpectedLinkText), RouteLinkFactory.create(route, false, false));
+        assertEquals(linkBuilder.build(), RouteLinkFactory.create(route, false, false));
     }
 
     @Test
     public void create_trueChooseDriving_returnCorrectUri() {
-        assertEquals(Uri.parse(baseExpectedLinkText + DRIVING_TRAVEL_MODE), RouteLinkFactory.create(route, true, false));
+        assertEquals(linkBuilder.onlyDriving().build(), RouteLinkFactory.create(route, true, false));
     }
 
     @Test
     public void create_trueSkipOverview_returnCorrectUri() {
-        assertEquals(Uri.parse(baseExpectedLinkText + SKIP_OVERVIEW), RouteLinkFactory.create(route, false, true));
+        assertEquals(linkBuilder.skipOverview().build(), RouteLinkFactory.create(route, false, true));
     }
 }
